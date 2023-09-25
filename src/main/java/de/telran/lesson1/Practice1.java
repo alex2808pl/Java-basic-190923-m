0ce40609f39172f35d8c361f8311c0c9d34038df
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-09-25 13:45:26 +0300

    Initial commit

---
+++ src/main/java/de/telran/lesson1/Practice1.java
@@ -0,0 +1,27 @@
+package de.telran.lesson1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("Вася");
+
+        System.out.print("Hello,");
+        System.out.print(" my");
+        System.out.print(" name");
+        System.out.print(" is");
+        System.out.println(" Вася");
+    }
+}
+
+/*    В методе main добавить инструкцию System.out.println(); необходимое количество раз, чтобы в консоль распечатался текст:
+        Hello,
+        my
+        name
+        is
+<your name>!
+        Используя инструкцию System.out.print(); распечатайте в консоль текст:
+        Hello, my name is <your name>!*/
+
