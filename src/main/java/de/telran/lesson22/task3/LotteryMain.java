f20b73f70bfcd3013d8889ed2ba58b2b82be0374
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-10-30 13:53:26 +0200

    lesson22-5

--- src/main/java/de/telran/lesson22/task3/LotteryMain.java
+++ src/main/java/de/telran/lesson22/task3/LotteryMain.java
@@ -8,16 +8,25 @@ import static de.telran.lesson8.RandomStudent.generateIntRandomInterval;
 
 public class LotteryMain {
     public static void main(String[] args) {
-        int[] userNumbers = Utils.getUserLotteryNumber();
-        System.out.println(Arrays.toString(userNumbers));
+        int count = 3;
+        int[][] userCountNumbers = new int[count] [COUNT_SET_SIZE];
+
+        for (int i = 0; i<count; i++) {
+            int[] userNumbers = Utils.getUserLotteryNumber();
+            userCountNumbers[i] = userNumbers;
+        }
+        System.out.println(Arrays.toString(userCountNumbers));
 
         Lottery lottery = new Lottery();
         lottery.runLottery();
         int[] lotteryNumbers = lottery.getWinnerNumbers();
         System.out.println(Arrays.toString(lotteryNumbers));
 
-        int countWinnerNumber = Utils.compareWinnerNumbers(lotteryNumbers, userNumbers);
-        System.out.println("Количество совпавших номеров - "+countWinnerNumber);
+        for (int i = 0; i<count; i++) {
+            int countWinnerNumber = Utils.compareWinnerNumbers(lotteryNumbers, userCountNumbers[i]);
+            System.out.println("Количество совпавших номеров - "+countWinnerNumber);
+        }
+
 
     }
 }
--- src/main/java/de/telran/lesson8/RandomStudent.java
+++ src/main/java/de/telran/lesson8/RandomStudent.java
@@ -17,7 +17,7 @@ public class RandomStudent {
          return generateNumber;
      }
 
-    static int generateIntRandomInterval(int min, int max) {
+    public static int generateIntRandomInterval(int min, int max) {
         Random random = new Random();
         int result = random.nextInt(max - min) + min;
         return result;
